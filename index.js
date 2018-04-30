f16709a8894b6f7ec515b3a47875b5716c350849
noelalfonsomiranda <noelalfonsomiranda@gmail.com>
2018-04-30 11:54:53 +0800

    convert es6 to js core, added skype share

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const width = 620
-const height = 360
-const leftPosition = (window.screen.width / 2) - ((width / 2) + 10)
-const topPosition = (window.screen.height / 2) - ((height / 2) + 50)
-const windowPosition = `left=${leftPosition},top=${topPosition},width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0,screenX=${leftPosition},screenY=${topPosition}`
+var width = 620;
+var height = 360;
+var leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+var topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+var windowPosition = 'left=' + leftPosition + ',' + 'top=' + topPosition + ',' + 'width=550,' + 'height=450,' + 'personalbar=0,toolbar=0,scrollbars=0,resizable=0,screenX=' + leftPosition + ',' + 'screenY=' + topPosition;
 
 // Facebook Share
-export const fbShare = (item, fbAppId) => {
+function fbShare (item, fbAppId) {
   // It's a global so need to read it from window
-  let FB = window.FB
+  var FB = window.FB;
 
-  const options = {
+  var options = {
     appId: fbAppId, // facebook developer ID
     status: true,
     autoLogAppEvents: true,
     xfbml: true, // parse social plugins on this page
     version: 'v2.10'
-  }
+  };
 
-  FB.init(options)
+  FB.init(options);
 
   if (FB) {
     FB.ui({
@@ -32,48 +32,81 @@ export const fbShare = (item, fbAppId) => {
           'og:image:alt': item.title
         }
       })
-    }, () => {
-      console.log('error')
-    })
+    }, function () {
+      console.log('error');
+    });
   }
 }
 
 // Twitter
-export const twitterShare = (item) => {
-  window.open(`http://twitter.com/share?url=${encodeURIComponent(item.url)}&text=${encodeURIComponent(item.text)}`, +'', windowPosition)
+function twitterShare (item) {
+  window.open('http://twitter.com/share?url=' + encodeURIComponent(item.url) + '&text=' + encodeURIComponent(item.text), + '', windowPosition);
 }
 
 // Google+
-export const googlePlusShare = (item) => {
-  window.open(`http://plus.google.com/share?url=${encodeURIComponent(item.url)}&text=${encodeURIComponent(item.text)}`, +'', windowPosition)
+function googlePlusShare (item) {
+  window.open('http://plus.google.com/share?url=' + encodeURIComponent(item.url) + '&text=' + encodeURIComponent(item.text), + '', windowPosition);
 }
 
 // LinkedIn
-export const linkedInShare = (item) => {
-  window.open(`http://www.linkedin.com/cws/share?url=${encodeURIComponent(item.url)}`, +'', windowPosition)
+function linkedInShare (item) {
+  window.open('http://www.linkedin.com/cws/share?url=' + encodeURIComponent(item.url), + '', windowPosition);
 }
 
 // Pinterest
-export const pinterestShare = (item) => {
-  window.open(`http://pinterest.com/pin/create/button/?url=${encodeURIComponent(item.url)}&media=${item.image}&description=${item.description}`, +'', windowPosition)
+function pinterestShare (item) {
+  window.open('http://pinterest.com/pin/create/button/?url=' + encodeURIComponent(item.url) + '&media=' + item.image + '&description=' + item.description, + '', windowPosition);
 }
 
 // Email
-export const emailShare = (item) => {
-  window.location.href = `mailto:?subject=${item.subject}&body=${item.body}`
+function emailShare (item) {
+  window.location.href = 'mailto:?subject=' + item.subject + '&body=' + item.body;
 }
 
 // Viber
-export const viberShare = (event, text) => {
-  window.location.href = 'https://3p3x.adj.st/?adjust_t=u783g1_kw9yml&adjust_fallback=https%3A%2F%2Fwww.viber.com%2F%3Futm_source%3DPartner%26utm_medium%3DSharebutton%26utm_campaign%3DDefualt&adjust_campaign=Sharebutton&adjust_deeplink=' + encodeURIComponent('viber://forward?text=' + encodeURIComponent(text + ' ' + window.location.href))
+function viberShare (event, text) {
+  window.location.href = 'https://3p3x.adj.st/?adjust_t=u783g1_kw9yml&adjust_fallback=https%3A%2F%2Fwww.viber.com%2F%3Futm_source%3DPartner%26utm_medium%3DSharebutton%26utm_campaign%3DDefualt&adjust_campaign=Sharebutton&adjust_deeplink=' + encodeURIComponent('viber://forward?text=' + encodeURIComponent(text + ' ' + window.location.href));
 }
 
 // WhatsApp
-export const whatsAppShare = (text) => {
-  window.location.href = `whatsapp://send?text=${text} ${window.location.href}`
+function whatsAppShare (text) {
+  window.location.href = 'whatsapp://send?text=' + text + window.location.href;
 }
 
 // Facebook Messenger
-export const fbMessengerShare = (appId) => {
-  window.location.href = `fb-messenger://share?link=${encodeURIComponent(window.location.href)}&app_id=${encodeURIComponent(appId)}`
+function fbMessengerShare (appId) {
+  window.location.href = 'fb-messenger://share?link=' + encodeURIComponent(window.location.href) + '&app_id=' + encodeURIComponent(appId);
+}
+
+// Skype for Web
+function skypeShare () {
+  (function(r, d, s) {
+    r.loadSkypeWebSdkAsync = r.loadSkypeWebSdkAsync || function(p) {
+      var js, sjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(p.id)) { return; }
+      js = d.createElement(s);
+      js.id = p.id;
+      js.src = p.scriptToLoad;
+      js.onload = p.callback
+      sjs.parentNode.insertBefore(js, sjs);
+    };
+    var p = {
+      scriptToLoad: 'https://swx.cdn.skype.com/shared/v/latest/skypewebsdk.js',
+      id: 'skype_web_sdk'
+    };
+    r.loadSkypeWebSdkAsync(p);
+  })(window, document, 'script');
+}
+
+module.exports = {
+  fbShare,
+  twitterShare,
+  googlePlusShare,
+  linkedInShare,
+  pinterestShare,
+  emailShare,
+  viberShare,
+  whatsAppShare,
+  fbMessengerShare,
+  skypeShare
 }
